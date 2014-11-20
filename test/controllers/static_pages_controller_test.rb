ccd299ab7e59933b3565369e5d4aa28b1e3f44f1
Joseph Romano <JDRomano2@gmail.com>
2014-11-20 11:38:39 -0500

    Worked on static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | VenomDB"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | VenomDB"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | VenomDB"
   end
 
 end
